1458bf571360cac7abeac062621e974acbf8c5f1
GabyCN <Gabynoestaquieta@gmail.com>
2020-04-23 18:14:38 +0200

    Con activity 2x4

--- app/src/main/java/com/example/memoria/MainActivity.kt
+++ app/src/main/java/com/example/memoria/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.memoria
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.*
@@ -18,155 +21,34 @@ import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
 
-    var mutableList: MutableList<Int> = mutableListOf()
-    var num: MutableList<Int> = mutableListOf(0)
-   // var cosa: MutableList<Int> = mutableListOf()
-   // var nombre: MutableList<View> = mutableListOf()
-    // var nuevaImagen: MutableList<Int> = mutableListOf()
-    val imagenes: MutableList<Int> = mutableListOf(R.drawable.la0,R.drawable.la1, R.drawable.la2, R.drawable.la3,
-        R.drawable.la0,R.drawable.la1, R.drawable.la2,R.drawable.la3)
-   // var prueba :Int = 0
-
-
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setListener()
-        btnREANUDAR.setOnClickListener {
-            reanudar(it)
-        }
-
-       imagenes.shuffle()
-    }
+        rgTodo.setOnCheckedChangeListener(
+            RadioGroup.OnCheckedChangeListener{ group, checkedId ->
+                val radio :RadioButton= findViewById(checkedId)
 
 
-    private fun setListener(){
-        var clickableViews: MutableList<View> =
-            mutableListOf(
-                tvBoxONE,
-                tvBoxTWO,
-                tvBoxThree,
-                tvBoxFOUR,
-                tvBoxFIVE,
-                tvBoxSIX,
-                tvBoxSEVEN,
-                tvBoxEIGHT
-            )
+                    when (radio.getId()) {
+                        R.id.rb2x2 -> {}
 
-        for(item in clickableViews) {
-            item.setOnClickListener{changeImage(it)}}
-    }
+                        R.id.rb2x3 ->{}
+
+                       else -> {val intent= Intent(this,memoria2x8::class.java)
+                        startActivity(intent)   }       }})
 
 
-    private fun changeImage(view: View) {
-
-        when (view.id) {
-            R.id.tvBoxONE -> {
-                view.setBackgroundResource(imagenes[0]); mutableList.add(0,0)
-            }
-            R.id.tvBoxTWO -> {
-                view.setBackgroundResource(imagenes[1]); mutableList.add(0,1)
-            }
-            R.id.tvBoxThree -> {
-                view.setBackgroundResource(imagenes[2]); mutableList.add(0,2)
-            }
-            R.id.tvBoxFOUR -> {
-                view.setBackgroundResource(imagenes[3]); mutableList.add(0,3)
-            }
-            R.id.tvBoxFIVE -> {
-                view.setBackgroundResource(imagenes[4]); mutableList.add(0,4)
-            }
-            R.id.tvBoxSIX -> {
-                view.setBackgroundResource(imagenes[5]); mutableList.add(0,5)
-            }
-            R.id.tvBoxSEVEN -> {
-                view.setBackgroundResource(imagenes[6]); mutableList.add(0,6)
-            }
-            else -> {
-                view.setBackgroundResource(imagenes[7]); mutableList.add(0,7)
-            }
         }
 
 
 
-        if (num.first() == 0) {
-            num.clear()
-            num.add(0,1)
-            //cosa.add(0,mutableList.last())
-
-
-
-            //Toast.makeText(this, "num: $num, lista $mutableList, prueba: $prueba", Toast.LENGTH_LONG).show()
-
-        } else {
-
-
-
-            if (imagenes[mutableList[0]] == imagenes[mutableList[1]]) {
-
-
-                num.clear()
-                num.add(0,0)
-
-                //Toast.makeText(this, " IF 1 num: $num lista: $mutableList ", Toast.LENGTH_LONG).show()
-
-            } else {
-
-
-                Handler().postDelayed(
-                    {
-                        tvBoxONE.setBackgroundResource(R.drawable.corazon)
-                        tvBoxTWO.setBackgroundResource(R.drawable.corazon)
-                        tvBoxThree.setBackgroundResource(R.drawable.corazon)
-                        tvBoxFOUR.setBackgroundResource(R.drawable.corazon)
-                        tvBoxFIVE.setBackgroundResource(R.drawable.corazon)
-                        tvBoxSIX.setBackgroundResource(R.drawable.corazon)
-                        tvBoxSEVEN.setBackgroundResource(R.drawable.corazon)
-                        tvBoxEIGHT.setBackgroundResource(R.drawable.corazon)
-                    },
-                    1000 // value in milliseconds
-                )
-
-
-
-
-               // Toast.makeText(this, " if 2 num: $num, lista: $mutableList ", Toast.LENGTH_LONG).show()
-                num.clear()
-                num.add(0,0)
-            }
-        }
-    }
-
-
-
-    fun reanudar(view: View) {
-        when (view.id) {
-            R.id.btnREANUDAR -> {
-                tvBoxONE.setBackgroundResource(R.drawable.corazon);
-                tvBoxTWO.setBackgroundResource(R.drawable.corazon);
-                tvBoxThree.setBackgroundResource(R.drawable.corazon);
-                tvBoxFOUR.setBackgroundResource(R.drawable.corazon);
-                tvBoxFIVE.setBackgroundResource(R.drawable.corazon);
-                tvBoxSIX.setBackgroundResource(R.drawable.corazon);
-                tvBoxSEVEN.setBackgroundResource(R.drawable.corazon);
-                tvBoxEIGHT.setBackgroundResource(R.drawable.corazon)
-            }
-
-        }
-
-        mutableList.clear()
-        mutableList.add(0,0)
-
-        num.clear()
-        num.add(0,0)
-
-        imagenes.shuffle()
 
     }
 
-}
+
+
 
 
 
---
+++ app/src/main/java/com/example/memoria/memoria2x2.kt
@@ -0,0 +1,12 @@
+package com.example.memoria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class memoria2x2 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_memoria2x2)
+    }
+}
